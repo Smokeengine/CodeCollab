1607ccf0a3f560507ed2774d7cdd759f5e0cbbcb
ANURAG VEMULA <101939604+Smokeengine@users.noreply.github.com>
2026-04-11 18:34:53 -0600

    Change socket connection URL to production server

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const {io} = require('socket.io-client');
-const socket = io('http://localhost:4000');
+const socket = io('https://codecollab-xoj8.onrender.com');
 
 socket.on('connect', () => {
     console.log('connected to the server, my id is: ' ,socket.id)
